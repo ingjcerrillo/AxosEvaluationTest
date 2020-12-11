87f6480b43421c6cc5ba5a7f57be2faa8935199c
J. Antonio Cerrillo Rivera <ing.jcerrillo@gmail.com>
2020-12-11 01:23:03 -0600

    Configured Product model into EFCore with InMemoryDB.

---
+++ Server/com.ToysAndGames.Server/com.ToysAndGames.Server.WebAPI/DAL/Context/ApiContext.cs
@@ -0,0 +1,29 @@
+﻿using com.ToysAndGames.Server.WebAPI.DAL.Mapping;
+using com.ToysAndGames.Server.WebAPI.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace com.ToysAndGames.Server.WebAPI.DAL.Context
+{
+    public partial class ApiContext : DbContext
+    {
+        
+        public virtual DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseInMemoryDatabase("ApiDb");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ProductMap());
+        }
+    }
+}
---
+++ Server/com.ToysAndGames.Server/com.ToysAndGames.Server.WebAPI/DAL/Mapping/ProductMap.cs
@@ -0,0 +1,38 @@
+﻿using com.ToysAndGames.Server.WebAPI.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace com.ToysAndGames.Server.WebAPI.DAL.Mapping
+{
+    public class ProductMap : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(e => e.Id);
+
+            builder.Property(e => e.Id)
+                .IsRequired();
+
+            builder.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(e => e.Description)
+                .HasMaxLength(100);
+
+            builder.Property(e => e.AgeRestriction)
+                .HasMaxLength(3);
+
+            builder.Property(e => e.Company)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(e => e.Price)
+                .HasMaxLength(4);
+        }
+    }
+}
