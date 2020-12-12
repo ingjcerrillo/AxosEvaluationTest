fdf46fe4fb5df1de51bb557d15349a80f7f20a13
J. Antonio Cerrillo Rivera <ing.jcerrillo@gmail.com>
2020-12-12 15:26:13 -0600

    Created default client angular project.

---
+++ Client/ToysAndGamesClient/src/app/models/product.model.ts
@@ -0,0 +1,12 @@
+import { DecimalPipe } from "@angular/common";
+
+export class Product {
+
+  public id!: number;
+  public name!: string;
+  public description!:string;
+  public ageRestriction!:string;
+  public company!:string;
+  public price!:number;
+
+}
