e58e6a8c68069de9bd420df32ad79cba93bd4072
J. Antonio Cerrillo Rivera <acerrillo@grupogrismar.com>
2020-12-14 11:16:36 -0600

    Added project for unit testing.

---
+++ Server/com.ToysAndGames.Server/com.ToysAndGames.Server.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace com.ToysAndGames.Server.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
